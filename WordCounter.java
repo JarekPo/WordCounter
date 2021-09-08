5d06d37404f03286e37a476a0eee24af8cc8558b
JarekPo <jarekpolowy@gmail.com>
2021-09-08 18:40:00 +0100

    First working version

--- WordCounter.java
+++ WordCounter.java
@@ -1,11 +1,25 @@
 public class WordCounter{
 	//vars
-
+	private String userText;
+	private int totalWords;
 	//constructor
-
+	public WordCounter(){
+		totalWords=1;
+	}
 	//set
-
+	public void setUserText(String userText){
+		this.userText=userText;
+	}
 	//compute
-
+	public void countWords(){
+		for(int i=1;i<userText.length();i++){
+			if((userText.charAt(i)==' ') && (userText.charAt(i-1)!=' ')){
+				totalWords++;
+			}
+		}
+	}
 	//get
+	public int getTotalWords(){
+		return totalWords;
+	}
 }
--- WordCounterApp.java
+++ WordCounterApp.java
@@ -1,17 +1,26 @@
+import javax.swing.JOptionPane;
 public class WordCounterApp{
 	public static void main(String args[]){
 		//vars
-
+		String userText, errorMsg;
+		int totalWords;
 		//object
-
+		WordCounter wc=new WordCounter();
 		//input
-
+		userText=(JOptionPane.showInputDialog(null, "Enter text")).toLowerCase();
+		if(userText.length()<2){
+			if(!(userText.contains("a") || userText.contains("i") || userText.contains("o"))){
+				errorMsg="Enter valid text";
+				JOptionPane.showMessageDialog(null, errorMsg);
+			}
+		}
 		//set
-
+		wc.setUserText(userText);
 		//compute
-
+		wc.countWords();
 		//get
-
+		totalWords=wc.getTotalWords();
 		//output
+		JOptionPane.showMessageDialog(null, "Number of words in your text: "+totalWords);
 	}
 }
