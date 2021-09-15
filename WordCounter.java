50d366cc5b3ed09c267f5b91c9a794dc839f0e17
JarekPo <jarekpolowy@gmail.com>
2021-09-15 17:41:20 +0100

    added most common letter counter

--- WordCounter.java
+++ WordCounter.java
@@ -1,12 +1,15 @@
 public class WordCounter{
 	//vars
 	private String userText;
-	private int totalWords, totalCharacters, totalSentences;
+	private int totalWords, totalCharacters, totalSentences, mostOccurrences, charOccurrences;
+	private char mostCommon;
 	//constructor
 	public WordCounter(){
 		totalWords=1;
 		totalCharacters=0;
 		totalSentences=0;
+		mostOccurrences=0;
+		charOccurrences=0;
 	}
 	//set
 	public void setUserText(String userText){
@@ -21,20 +24,35 @@ public class WordCounter{
 		}
 	}
 	public void countCharacters(){
-		for(int i=0; i<userText.length();i++){
+		/*for(int i=0; i<userText.length();i++){
 			totalCharacters++;
-		}
+		}*/
+		totalCharacters=userText.length();
 	}
 	public void countSentences(){
 		if(userText.length()>0){
 			for(int i=1;i<userText.length();i++){
-				if(((userText.charAt(i)=='.') || (userText.charAt(i)=='!') || (userText.charAt(i)=='?')) && ((userText.charAt(i-1)!='.') || (userText.charAt(i)!='!') || (userText.charAt(i)!='?'))){
+				if(((userText.charAt(i)=='.') || (userText.charAt(i)=='!') || (userText.charAt(i)=='?')) && ((userText.charAt(i-1)!='.') && (userText.charAt(i-1)!='!') && (userText.charAt(i-1)!='?'))){
 					totalSentences++;
 				}
 			}
 		}
 		if (totalSentences==0){
 			totalSentences=1;
+		}
+	}
+	public void findMostCommonLetter(){
+		for(int i=0;i<userText.length();i++){
+			for(int j=(i+1);j<userText.length();j++){
+				if(userText.charAt(i)==userText.charAt(j)){
+					charOccurrences++;
+				}
+			}
+			if(charOccurrences>=mostOccurrences){
+				mostCommon=userText.charAt(i);
+				mostOccurrences=charOccurrences;
+				charOccurrences=0;
+			}
 		}
 	}
 	//get
@@ -46,5 +64,11 @@ public class WordCounter{
 	}
 	public int getSentences(){
 		return totalSentences;
+	}
+	public int getMostOccurrences(){
+		return mostOccurrences;
+	}
+	public char getMostCommonLetter(){
+		return mostCommon;
 	}
 }
